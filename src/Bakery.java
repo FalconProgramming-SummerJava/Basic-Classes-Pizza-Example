802d1c62132edce89158b7babecee08a8f6f462a
TheGuyWithTheFace <thescratcherman@gmail.com>
2015-06-04 20:22:25 -0400

    Added comments

--- src/Bakery.java
+++ src/Bakery.java
@@ -1,8 +1,19 @@
 public class Bakery {
+	// This special method starts everything
 	public static void main(String[] args) {
+		// Create an object of the pizza class with Cheese and Pepperoni toppings
 		String[] toppings = {"Cheese", "Pepperoni"};
 		Pizza perrysPizza = new Pizza(toppings);
 
+		// Call the printToppings method every pizza has to list this object's toppings.
 		perrysPizza.printToppings();
+		
+		// What would happen if you created a new pizza with different toppings?
+		String[] newToppings = {/* insert toppings here */};
+		Pizza otherPizza = new Pizza(newToppings);
+		
+		// What happens if we call printToppings on the new Pizza,
+		// and then call it on the original pizza object we created?
+		// Take your best guess, then try it out!
 	}
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -1,12 +1,20 @@
+// Create new class (blueprint for) Pizza
 public class Pizza {
+	// Any Pizza object has a toppings property, an array of strings
 	public String[] toppings = new String[5];
 	
+	// This is the Constructor, called whenever an object is made of this class
+	// The constructor expects an array of strings to be passed to it.
 	public Pizza(String[] toppingNames) {
+		// take the toppings the user gave us and save it to our new object
 		toppings = toppingNames;
+		
 		System.out.println("Pizza made!");		
 	}
 	
+	// This is a method, any created object will have this method to be called manually
 	public void printToppings() {
+		// Loop through all the toppings, printing each
 		for(int i = 0; i < toppings.length; i++) {
 			System.out.println(toppings[i]);
 		}
